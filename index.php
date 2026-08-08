b48d5696a8ba52433ec825471eb2929e636bcb7f
Unite_Entity <hackathonssp85@gmail.com>
2026-08-08 21:59:00 +0545

    Autism Project Pushing Started

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// index.php
+require_once __DIR__ . '/includes/config.php';
+
+$pageTitle = "AutiLearn AI | Empowering Neurodiverse Learning";
+
+// Include Layout Components
+require_once __DIR__ . '/includes/header.php';
+require_once __DIR__ . '/pages/home.php';
+require_once __DIR__ . '/includes/footer.php';
+?>
